c489951d3064db3dc2693a4c7240b99b9e7ca78f
andydeangelis <andy.deangelis@live.com>
2018-07-12 15:28:22 -0400

    Documentation

---
+++ ScheduledTask.ps1
@@ -0,0 +1,11 @@
+﻿$ServerFileName = "C:\Scripts\test\myservers.txt"
+$ReportPath = "C:\Scripts\Test\"
+$domainCred = "C:\Scripts\test\domainCred.XML"
+
+$scriptFile = "HotFixReport.ps1 -ServerFileName $ServerFileName -ReportPath $ReportPath -DomainCredXMLFile $domainCred -SaveCreds -RunSilent"
+
+$argumentList = "-executionpolicy bypass", "-mta", "-noninteractive", "-windowstyle normal", "-nologo", "-file $scriptFile"
+
+# Start-Process powershell -WorkingDirectory $PSScriptRoot -ArgumentList $argumentList -NoNewWindow
+
+.\HotFixReport.ps1 -ServerFileName $ServerFileName -ReportPath $ReportPath -DomainCredXMLFile $domainCred -SaveCreds -RunSilent
